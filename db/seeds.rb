ad48cacf56c82bbb20ec881af3bc6f6284df2add
emiliano-ma <emilianomainero@gmail.com>
2020-11-02 20:14:16 +0100

    Adds assignments to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Assignment.destroy_all
+
+Assignment.create(
+  [
+    {
+      title: "Build a Web page",
+      skills: ["Ruby", "JavaScript"],
+      points: 320,
+      budget: 500,
+    },
+    {
+      title: "Build an API",
+      skills: ["Ruby", "Rails"],
+      points: 670,
+      budget: 1500,
+    }, {
+      title: "Build a Native App",
+      skills: ["React Native", "JavaScript"],
+      points: 1320,
+      budget: 2500,
+    }, {
+      title: "Build a Home page",
+      skills: ["HTML", "JavaScript", "CSS"],
+      points: 220,
+      budget: 300,
+    },
+  ]
+)
